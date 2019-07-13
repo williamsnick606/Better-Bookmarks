f1c5c94c15b3bec67b622bb3c3e89f302723a228
BarryManifold <bjmcgrat@ucsc.edu>
2019-07-13 15:16:56 -0700

    Renamed addFoldersToPopup function to addBookmarkContent and I fixed it to properly construct the HTML for folder dropdowns that contain the bookmarks and subfolders withing them.

--- better_bookmarks/js/utils.mjs
+++ better_bookmarks/js/utils.mjs
@@ -12,8 +12,8 @@
  * @return undefined
  *
  */
-export function addFoldersToPopup() {
-    var text, bmarks;
+export function addBookmarkContent() {
+    var folderText, bmarkText, search;
     /*
     * Walks the bookmark tree adding
     * list tags for each folder it finds
@@ -29,29 +29,92 @@ export function addFoldersToPopup() {
             return;
         }
         for (let i = 0; i < bs.length; i++) {
-            let bmark = bs[i];
-            let cs    = bmark.children;
-            if (bmark.id != 0 && bmark.url == undefined) {
-                text += "<li id=" + bmark.id +
-                       "><a href=\"#\">";
+            let node = bs[i];
+            // Folder case.
+            if (node.id != 0 && node.url == undefined) {
+                // Create new folder dropdown.
+                folderText += "<div id=\"folder" +
+                              node.id + "\" class=\"dropdown\">" +
+                              "<a name=\"" +
+                              node.title +
+                              "\" class=\"dropbtn\" " +
+                              "href=\"#\">";
                 // Chrome allows untitled folders, so
                 // check for them.
-                if (bmark.title.length > 0) {
-                    text += bmark.title + "</a></li>";
+                if (node.title.length > 0) {
+                    folderText += node.title + "</a>";
                 }
                 // Untitled bookmark folder.
                 else {
-                    text += "untitled</a></li>";
+                    folderText += "untitled</a>";
                 }
             }
-            walkChildren(bmark.children);
+            // Bookmark case.
+            if (node.url != undefined && node.id != 0) {
+                // Only create a bookmark dropdown if
+                // the parent folder isn't the root,
+                // i.e., a parentId of zero.
+                if (node.parentId != undefined &&
+                    node.parentId != 0) {
+                    
+                    // Create a new bookmark "list."
+                    if (bmarkText.length == 0) {
+                        bmarkText += "<div id=\"folderDropdown" +
+                                     node.id + "\" " +
+                                     "class=\"dropdown-content\">";
+                    }
+                }
+                // Create a new bookmark entry.
+                bmarkText += "<a id=\"bookmark" + node.id +
+                             "\" href=\"#\">" + node.title +
+                             "</a>";
+            }
+            walkChildren(node.children);
+        }
+        // Close out the bookmark list and folder dropdown,
+        // then reset it.
+        if (bmarkText != 0) {
+            folderText += bmarkText + "</div></div>";
+            bmarkText   = "";
         }
     }
-    bmarks = document.getElementById('folders');
+    // Get the search bar div so we can place our
+    // folder dropdowns after it.
+    search = document.getElementById("search");
+    // Create the necessary HTML and add it to the DOM.
     chrome.bookmarks.getTree(function(bs) {
-        text = "<ul id=\"bookmarkList\">";
+        folderText = "";
+        bmarkText  = "";
         walkChildren(bs);
-        text = text + "</ul>";
-        bmarks.innerHTML = text;
+        search.insertAdjacentHTML("afterend", folderText);
     });
+}
+
+/*
+ * Validates the search bar text and if the
+ * input is valid, passes it to a callback
+ * that searches for that text in the bookmarks.
+ *
+ * ==== Still in progress. ====
+ *
+ */
+export function validateForm() {
+    var val = document.forms["searchForm"]["searchBar"].value;
+    alert(val);
+}
+
+function toggleBookmarks() {
+    document.getElmentById("folderDropdown")
+            .classList
+            .toggle("show");
+}
+
+export function addBookmarksToPopup() {
+    const folderList = document.getElementById("folderList");
+    alert(folderList);
+    const folders    = folderList.childNodes;
+    for (let i = 0; i < folders.length; i++) {
+        var folder = folders[i];
+        folder.onclick = toggleBookmarks;
+    }
 }
