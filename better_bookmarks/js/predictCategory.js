7de57c7e752b836ecadeafc3be600227df3baa33
Nicholas Williams <williamsnick606@gmail.com>
2019-07-14 05:12:05 -0700

    Adding function definition to predict Bookmark category

---
+++ better_bookmarks/js/predictCategory.js
@@ -0,0 +1,9 @@
+/*
+ * Description: This function predicts which category a bookmark belongs to given the website title and description.
+ * Inputs: "title" == a string containing the website title, 
+ * 	   "description" == a string containing the description of the website
+ * Output: A number 0-12 corresponding to one of the 13 categories
+*/
+function predictCategory(title, description){
+
+}
