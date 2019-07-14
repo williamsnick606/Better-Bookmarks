cb54f9c094186fe0826008cf63204b0b9c2914a8
Nicholas Williams <npwillia@ucsc.edu>
2019-07-14 14:08:20 -0700

    updated the function parameters
    we no longer needed titles as an input so it was removed, and the range of outputs now reflects our 5 best performing categories

--- better_bookmarks/js/predictCategory.js
+++ better_bookmarks/js/predictCategory.js
@@ -1,9 +1,8 @@
 /*
  * Description: This function predicts which category a bookmark belongs to given the website title and description.
- * Inputs: "title" == a string containing the website title, 
- * 	   "description" == a string containing the description of the website
- * Output: A number 0-12 corresponding to one of the 13 categories
+ * Inputs: "description" == a string containing the description of the website
+ * Output: A number 0-4 corresponding to one of the 13 categories
 */
-function predictCategory(title, description){
+function predictCategory(description){
 
 }
