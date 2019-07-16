bf803eff82e971f855b7015ec5250547abfedecc
Kyle Head <krhead@ucsc.edu>
2019-07-16 11:49:24 -0700

    progress 7/16. name field working in modal

--- better_bookmarks/js/modal.js
+++ better_bookmarks/js/modal.js
@@ -4,22 +4,51 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get the button that creates a bookmark
+var btn2 = document.getElementById("makeBtn");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// This will be the tabs title for bookmark naming purposes
+var usableT;
+// This will be the tabs description
+//var usableD;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+//Get access to 
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+	var tab = tabs[0];
+	var title = tab.title;
+	//var desc = tab.description
+	usableT = title;
+	//usableD = desc;
+	autofiller(usableT)
+})
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+// All the modal functionality has to be a callback function from the chrome.tabs.query
+function autofiller(usableT) {
+	// When the user clicks the 'Bookmark' button,
+	btn.onclick = function() {
+		// Display the modal
+    	modal.style.display = "block";
+    	// Autofill the site Title as the bookmark name
+    	document.getElementById("newName").value = usableT;
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+
+	// When a user clicks the Create button, save a new bookmark
+	btn2.onclick = function() {
+
+	}
 }
