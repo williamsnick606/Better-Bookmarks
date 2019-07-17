8592dd05fe2a695ac949319b67d25f0096209448
LeBob007 <52551319+LeBob007@users.noreply.github.com>
2019-07-17 16:35:52 -0700

    Update content_script.js

--- better_bookmarks/js/content_script.js
+++ better_bookmarks/js/content_script.js
@@ -1,22 +1,11 @@
-var port = chrome.runtime.connect();
-
-window.addEventListener("message", (event) => {
-    if (event.source != window)
-        return;
-
-    if (event.data.type && (event.data.type == "FROM_PAGE")) {
-        console.log("Content script received: " + event.data.text);
-        port.postMessage(event.data.text);
-    }
-}, false);
-
 const title = document.getElementsByTagName("title")[0];
 const metas = document.getElementsByTagName("meta");
 const url = document.URL;
+var description;
 for (let i = 0; i < metas.length; i++) {
     const meta = metas[i];
     if (meta.name.toLowerCase() == "description"){
-        var description = meta.content;
+        description = meta.content;
     }
 }
 if(description === undefined){
