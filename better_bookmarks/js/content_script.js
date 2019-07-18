08e3622ab7bd08fb149e192783f77299d4355ba2
LeBob007 <52551319+LeBob007@users.noreply.github.com>
2019-07-17 19:16:36 -0700

    Update content_script.js

--- better_bookmarks/js/content_script.js
+++ better_bookmarks/js/content_script.js
@@ -1,11 +1,7 @@
 const title = document.getElementsByTagName("title")[0];
 const metas = document.getElementsByTagName("meta");
 const url = document.URL;
-<<<<<<< HEAD
-var description = undefined;
-=======
 var description;
->>>>>>> 7956320f75931d32b948ac49708c833c2fc07849
 for (let i = 0; i < metas.length; i++) {
     const meta = metas[i];
     // Some websites don't conform to the norm, thus requiring toLowerCase()
@@ -18,9 +14,4 @@ if(description === undefined){
 }
 else{
     chrome.storage.sync.set({title: title.innerText, desc: description, url: url}, function () {});
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> 7956320f75931d32b948ac49708c833c2fc07849
