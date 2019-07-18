19e8d477959539bd53080725df341a2b07f7b37c
Kyle Head <krhead@ucsc.edu>
2019-07-17 23:37:00 -0700

    progress 7/17. name and folder functionality, working on prediction

--- better_bookmarks/js/modal.js
+++ better_bookmarks/js/modal.js
@@ -10,12 +10,64 @@ var btn2 = document.getElementById("makeBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// This will be the tabs title for bookmark naming purposes
+// This will be the tabs Title for bookmark naming purposes
 var usableT;
-// This will be the tabs body for folder choosing
+// This will be the tabs Body for folder choosing
 var usableU;
-// This will be the tabs description
-//var usableD;
+// This will be the tabs Description
+var usableD;
+// This variable is the dropdown menu that houses all folder options
+var select = document.getElementById('folderDrop');
+// These will help keep track of the folder ID's of the folders added
+// by the extensioon so the ML predictor knows which folder to 
+//use as a recommendation
+var arts, business, health, society, sports;
+// This adds all bookmark folders as options in the modal's folder
+// selection dropdown. '0' means it starts @ the root node
+grabFolders('0');
+function grabFolders(id) {
+    // Gets each bookmark item by id
+    chrome.bookmarks.getChildren(id, function(children) {
+        children.forEach(function(bookmark) { 
+            // If the url is null (it's a folder)
+            if(bookmark.url == null){
+                // Create an option to add to our dropdown
+                var option = document.createElement('option');
+                // Copy the info over from folder to the new option
+                option.value = bookmark.id;
+                option.text = bookmark.title;
+                // When we run into a predictable folder, make note
+                // of the folder's ID
+                if(option.text == "Arts") {
+                    arts = option.value;
+                } else if(option.text == "Business") {
+                    business = option.value;
+                } else if(option.text == "Health") {
+                    health = option.value;
+                } else if(option.text == "Society") {
+                    society = option.value;
+                } else if(option.text == "Sports") {
+                    sports = option.value;
+                }
+                // Add the option to the dropdown
+                select.appendChild(option);
+            }
+            // Recursive call on the folder's children
+            grabFolders(bookmark.id);
+        });
+    });
+}
+
+chrome.storage.sync.get(["title", "url", "desc"], function(result) {
+    usableT = result.title;
+    usableU = result.url;
+    usableD = result.desc;
+    var o = ["art", "business", "health", "society", "sports"];
+    var p = predictCategory(usableT, usableD);
+    var prediction = o[p];
+    autofiller(usableT, usableU, /*prediction*/);
+});
+
 
 //Get access to
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -29,14 +81,27 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     autofiller(usableT, usableU);
 })
 
+
 // All the modal functionality has to be a callback function from the chrome.tabs.query
-function autofiller(usableT, usableU) {
+function autofiller(usableT, usableU, prediction) {
     // When the user clicks the 'Bookmark' button,
     btn.onclick = function() {
         // Display the modal
         modal.style.display = "block";
         // Autofill the site Title as the bookmark name
         document.getElementById("newName").value = usableT;
+        // Autofill the category prediction as the folder preset
+        if(prediction == "art") {
+            document.getElementById("folderDrop").value = arts;
+        } else if (prediction == "business") {
+            document.getElementById("folderDrop").value = business;
+        } else if (prediction == "health") {
+            document.getElementById("folderDrop").value = health;
+        } else if (prediction == "society") {
+            document.getElementById("folderDrop").value = society;
+        } else if (prediction == "sports") {
+            document.getElementById("folderDrop").value = sports;
+        }
     }
 
     // When the user clicks on <span> (x), close the modal
@@ -51,11 +116,15 @@ function autofiller(usableT, usableU) {
         }
     }
 
-    var bookURL = document.getElementById("newName");
-    // When a user clicks the Create button, save a new bookmark
+    // When a user clicks the Create button
     btn2.onclick = function() {
-        chrome.bookmarks.create({'parentId': null,
-                                 'title':    usableT,
+        //Grab the current bookmark name
+        var bookT = document.getElementById("newName").value;
+        //Grab the current bookmark folder
+        var selection = document.getElementById("folderDrop").text;
+        var ID = document.getElementById("folderDrop").value;
+        chrome.bookmarks.create({'parentId': ID,
+                                 'title':    bookT,
                                  'url':      usableU
         });
     }
