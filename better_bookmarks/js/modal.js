cfa24ce010ad6a1f479ea73e71e325f0f6cc35b7
BarryManifold <52549693+BarryManifold@users.noreply.github.com>
2019-07-19 23:47:40 -0700

    Revert of Kyle's Merge (#26). (#27)
    * Update issue templates
    
    Adding issue tracking to this repo, because there's no reason it shouldn't be enabled.  Honestly, I don't know why it wasn't enabled to begin with; I'm pretty sure issue tracking is automatically enabled, so it would have had to have been manually disabled, but maybe I'm wrong.
    
    * Adding attributes for git lfs.
    
    * Updating git lfs locations.
    
    * Revert "kyle's 7/19 merge (#26)"
    
    This reverts commit 712bab1d09e9c2d9248ba44dee22078cfe0bc43d.
    
    Reverting this merge because it puts the upstream/master extension into an error state.

--- better_bookmarks/js/modal.js
+++ better_bookmarks/js/modal.js
@@ -7,138 +7,61 @@
  */
 import {predictCategory} from './predictCategory.mjs'
 
-// Make variable's for the modal and all of its buttons
-var modal    = document.getElementById("myModal");
-var newBook  = document.getElementById("newBook");
-var saveBook = document.getElementById("saveBook");
-var span     = document.getElementsByClassName("close")[0];
-var select   = document.getElementById('folderDrop');
-// These will help keep track of the folder ID's of the folders added
-// by the extensioon so the ML predictor knows which folder to 
-//use as a recommendation
-var art, business, health, society, sports;
-// This adds all bookmark folders as options in the modal's folder
-// selection dropdown. '0' means it starts @ the root node
-grabFolders('0');
-function grabFolders(id) {
-    // Gets each bookmark item by id
-    chrome.bookmarks.getChildren(id, function(children) {
-        children.forEach(function(bookmark) { 
-            // If the url is null (it's a folder)
-            if(bookmark.url == null){
-                // Create an option to add to our dropdown
-                var option = document.createElement('option');
-                // Copy the info over from folder to the new option
-                option.value = bookmark.id;
-                option.text = bookmark.title;
-                // When we run into a predictable folder, make note
-                // of the folder's ID
-                if(option.text == "Art") {
-                    art = option.value;
-                } else if(option.text == "Business") {
-                    business = option.value;
-                } else if(option.text == "Health") {
-                    health = option.value;
-                } else if(option.text == "Society") {
-                    society = option.value;
-                } else if(option.text == "Sports") {
-                    sports = option.value;
-                }
-                // Add the option to the dropdown
-                select.appendChild(option);
-            }
-            // Recursive call on the folder's children
-            grabFolders(bookmark.id);
-        });
-    });
-}
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// This will be the tabs Title for bookmark naming purposes
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the button that creates a bookmark
+var btn2 = document.getElementById("makeBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// This will be the tabs title for bookmark naming purposes
 var usableT;
+// This will be the tabs body for folder choosing
 var usableU;
+// This will be the tabs description
 var usableD;
-var o = ["art", "business", "health", "society", "sports"];
-var prediction;
 
-
-chrome.storage.sync.get(["title", "url", "desc"], function(result) {
+chrome.storage.sync.get(["title", "desc", "url"], function(result) {
     usableT = result.title;
     usableU = result.url;
     usableD = result.desc;
     console.log("Getting title, description, and url " +
                 "inside modal...");
     console.log("title = " + result.title + " | " +
-                "desc  = " + result.desc);
-    predictCategory(usableT, usableD).then(function(p) {
-        prediction = o[p];
-    });
+                "desc = " + result.desc);
     autofiller(usableT, usableU);
 });
 
-// I'm keeping my data grabbing function here until Roberts is able to update itself
-// automatically instead of on a page refresh
-/*
-//Get access to
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var tab = tabs[0];
-    var title = tab.title;
-    var url = tab.url;
-    //var body = tab.title;   // document.queryselector('body').innertext
-    usableT = title;
-    usableU = url;
-    //usableB = body;
-    autofiller(usableT, usableU);
-})
-*/
-
 // All the modal functionality has to be a callback function from the chrome.tabs.query
-function autofiller(usableT, usableU, prediction) {
-
-    // Autofill the site Title as the bookmark name
-    document.getElementById("newName").value = usableT;
-
-    // Autofill the category prediction as the folder preset
-    if(prediction == 'art') {
-        document.getElementById("folderDrop").value = art;
-    } else if (prediction == 'business') {
-        document.getElementById("folderDrop").value = business;
-    } else if (prediction == 'health') {
-        document.getElementById("folderDrop").value = health;
-    } else if (prediction == 'society') {
-        document.getElementById("folderDrop").value = society;
-    } else if (prediction == 'sports') {
-        document.getElementById("folderDrop").value = sports;
-    }
-
+function autofiller(usableT, usableU) {
     // When the user clicks the 'Bookmark' button,
-    newBook.onclick = function() {
+    btn.onclick = function() {
         // Display the modal
         modal.style.display = "block";
+        // Autofill the site Title as the bookmark name
+        document.getElementById("newName").value = usableT;
     }
-
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
-
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
-
-    // When a user clicks the Create button
-    saveBook.onclick = function() {
-        // Grab the current bookmark name
-        var bookT = document.getElementById("newName").value;
-        // Grab the current bookmark folder
-        var selection = document.getElementById("folderDrop").text;
-        var ID = document.getElementById("folderDrop").value;
-        // Save the bookmark
-        chrome.bookmarks.create({'parentId': ID,
-                                 'title':    bookT,
+    var bookURL = document.getElementById("newName");
+    // When a user clicks the Create button, save a new bookmark
+    btn2.onclick = function() {
+        chrome.bookmarks.create({'parentId': null,
+                                 'title':    usableT,
                                  'url':      usableU
-        });
+                                });
     }
 }
