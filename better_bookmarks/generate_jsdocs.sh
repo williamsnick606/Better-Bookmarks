85b19b70bc67c26edeba4e9c36f91d371b6245a8
BarryManifold <bjmcgrat@ucsc.edu>
2019-07-20 03:22:54 -0700

    Bash/shell script for running jsdoc on JS source.  Currently the JS source files are hardcoded in an array in this file, but I may just have it run on all the files in the js folder even though not all of them have functions, but I'll probably just keep it hardcoded and add the source files in js/ with functions to the array in this file.

---
+++ better_bookmarks/generate_jsdocs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# file        : generate_jsdocs.sh
+# description : This shell script runs the automantic JavaScript
+#               documentation generation tool `jsdoc' on all the
+#               proper files in the js directory.
+
+# Destination folder for JSDoc.
+dest="./docs"
+
+# Array of files to run `jsdoc' on.
+jsFiles=("js/utils.mjs")
+
+# Loop through jsFiles and call `jsdoc' on each file.
+for jsFile in "${jsFiles[@]}"; do
+    inJSFile="$jsFIle"
+    if [[ "$jsFile" == *".mjs" ]]; then
+        inJSFile="${jsFile%*.mjs}.js"
+        echo "Copying $jsFile to $inJSFile..."
+        cp -v "$jsFile" "$inJSFile"
+        echo "Running jsdoc on file \"$inJSFile\"..."
+    fi
+    jsdoc --destination "$dest" "$inJSFile"
+    if [ "$jsFile" != "$inJSFile" ]; then
+        echo "Removing temporary js file \"$inJSFile\"..."
+        rm -v "$inJSFile"
+    fi
+done
