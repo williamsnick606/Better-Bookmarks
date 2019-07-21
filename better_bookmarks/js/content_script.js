7d9666655a22fddb7115a134c40a0bebe2fe417a
BarryManifold <52549693+BarryManifold@users.noreply.github.com>
2019-07-20 20:27:44 -0700

    Execute script (#30)
    * Update issue templates
    
    Adding issue tracking to this repo, because there's no reason it shouldn't be enabled.  Honestly, I don't know why it wasn't enabled to begin with; I'm pretty sure issue tracking is automatically enabled, so it would have had to have been manually disabled, but maybe I'm wrong.
    
    * Adding attributes for git lfs.
    
    * Updating git lfs locations.
    
    * Removed js/add_bookmarks b/c it's no longer used/needed.
    
    * Deleting js/getTitleDesc b/c we switched to content scripts, so it's useless now.
    
    * Cleanded up some of the formatting.
    
    * Simplified the update state code.
    
    * I think I removed a newline at EOF.
    
    * Cleaned up some of the formatting.
    
    * Added grabbing the description.
    
    * Cleaned up formatting.
    
    * Added font CSS, specifically for using a Google font.
    
    * Removed check for child array length of 0 in order to get proper dropdown stack popping.
    
    * Added a link to a Google font.
    
    * Reverting back to previous call to chrome.storage.sync.set(...) where a conditional check on description is made, because Chrome acts silly by not updating, or deleting, a key in storage to undefined when you set it as undefined.
    
    * Making js/predictCategory.mjs an ES6 module file for easy import in js/modal.js.
    
    * Added description comment at top of file.
    
    * Added description comment at top of file and added an import of the preditCategory/2 function located in js/predictCategory.mjs.
    
    * Removing js/predictCategory.js because it has been renamed to js/predictCategory.mjs.
    
    * Adding tensor flow code in script tag.
    
    * Added an event listener for tab changes, but that's going to be removed.
    
    * Temporarily commented out the call to the chrome storage api's get function.
    
    * Added a tabs permission.
    
    * The dict variable was missing a preceding var, let, or const keyword where it was defined, which is a syntax error.  Therefore, I put const before dict where it is defined.
    
    * Just a note about a temporary fix to the multiple tab bookmarking problem.
    
    * Added file and description comment that also includes non-optimal solution to new tab issue.
    
    * Switched to using === instead of ==.
    
    * Switched to using === instead of ==, and I also removed explicit undefined equality checks.
    
    * Just defining dict is an undefined reference error in strict mode.  Even not in strict mode, not delaring variables is bad practice.  I've made dict a const variable to fix the error.
    
    * Copied over the js/utils.mjs version in my handle_nested_contents branch over here, to my clean_up branch to fix issues caused by switching from == and != over to === and !==.
    
    * Made comment about how the file isn't being used currently.
    
    * Removed content to get the file back to only containing context menu code.
    
    * Modal now uses executeScript to fix the new tab problem.
    
    * Removed the content script section.
    
    * Added note about how this file isn't currently being used.
    
    * Fixed syntax error (added missing + sign in console.log message).
    
    * Rearranged code, so there are less globals and variable name shadowing.
    
    * Turned alert into console.log and made the printed message more informative.

--- better_bookmarks/js/content_script.js
+++ better_bookmarks/js/content_script.js
@@ -4,57 +4,36 @@
  *               available, the title, description, and url of
  *               the website that the user has currently visited.
  *
- * NOTE: When a user opens a tab, opens another tab, and then
- *       switches back to the first tab, the page information
- *       for the first tab is lost.
- *
- *       A non-optimal temporary solution to this, is saving
- *       each tab id and associating the page data with the
- *       corresponding tab id.  For example,
- *
- *           chrome.storage.sync.set({ _tabId: { title: pageTitle
- *                                               ...
- *                                             }
- *                                   });
- *
- *       Then in modal.js, we would run:
- *
- *           
- *           let currentTabId;
- *           chrome.tabs.query({ active: true, currentWindow: true},
- *                             (tabs) => {
- *               let currentTab = tabs[0];
- *               currentTabId   = currentTab.id;
- *           });
- *           let pageTitle;
- *           let pageURL;
- *           ...
- *           chrome.storage.sync.get(["_" + currentTabId],
- *                                   (result) => {
- *               pageTitle = result.title
- *               pageURL   = result.url
- *               ...
- *           });
- *           ...
+ * NOTE: This file is not currently used.
  *
  */
+
 const title = document.getElementsByTagName("title")[0];
+const url   = document.URL;
 const metas = document.getElementsByTagName("meta");
-const url = document.URL;
-var description;
+
+let pageDescription;
 for (let i = 0; i < metas.length; i++) {
     const meta = metas[i];
     // Some websites don't conform to the norm, thus requiring toLowerCase()
     if (meta.name.toLowerCase() == "description"){
-        description = meta.content;
+        pageDescription = meta.content;
     }
 }
+
 console.log("Got title = " + title + " | " +
-            "description = " + description);
-if(!description){
-    description = "";
+            "description = " + pageDescription);
+
+/*
+if(!pageDescription){
+    pageDescription = "";
 }
 chrome.storage.sync.set({ title: title.innerText
                         , desc: description
                         , url: url
                         });
+*/
+{ title       : pageTitle
+, description : pageDescription
+, url         : pageURL
+};
--- better_bookmarks/js/modal.js
+++ better_bookmarks/js/modal.js
@@ -20,41 +20,74 @@ var btn2 = document.getElementById("makeBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// This will be the tabs title for bookmark naming purposes
-var usableT;
-// This will be the tabs body for folder choosing
-var usableU;
-// This will be the tabs description
-var usableD;
 
-chrome.storage.sync.get(["title", "desc", "url"], function(result) {
-    usableT = result.title;
-    usableU = result.url;
-    usableD = result.desc;
-    console.log("Getting title, description, and url " +
-                "inside modal...");
-    console.log("title = " + result.title + " | " +
-                "desc = " + result.desc);
-    //document.write(usableT);
-    predictCategory(usableT).then(function(t){
+/* 
+ * Since executeScript doesn't seem to like when
+ * I set the file property with content equivalent
+ * to what follows below, we have to resort to the
+ * below solution until we figure out how to use a
+ * file with the exact same content, or an even better
+ * solution that doesn't require the use of executeScript.
+ */
+const codeToExecute =
+    "const pageTitle = document.getElementsByTagName(\"title\")[0].innerText;\n" +
+    "const pageURL   = document.URL;\n" +
+    "const metas     = document.getElementsByTagName('meta');\n" +
+    "let pageDescription;\n" +
+    "for (let i = 0; i < metas.length; i++) {\n" +
+    "    const meta = metas[i];\n" +
+        // Some websites don't conform to the norm,
+        // thus requiring toLowerCase().
+    "    if (meta.name.toLowerCase() === 'description'){\n" +
+    "        pageDescription = meta.content;\n" +
+    "    }\n" +
+    "}\n" +
+    "const pageData = {title: pageTitle, description: pageDescription, url: pageURL};\n" +
+    "pageData;";
 
-  // do something ............. <-------------------
-
-    var o = ["art", "business", "health", "society", "sports"];
-    //document.write(o[t]);
+// Collect the relevant page information for the
+// tab that "add bookmark" was clicked on.
+chrome.tabs.executeScript({ code  : codeToExecute
+                          , runAt : "document_end"
+                          }, (results) => {
+    // This will be the tabs title for bookmark naming purposes
+    let usableT;
+    // This will be the tabs body for folder choosing
+    let usableU;
+    // This will be the tabs description
+    let usableD;
+    const result = results[0];
+    usableT      = result.title;
+    usableU      = result.url;
+    usableD      = result.description;
+    console.log("title = " + usableT + "\n" +
+                "url = " + usableU + "\n" +
+                "description = " + usableD);
+    // Fill the title and URL fields that the
+    // user sees.
     autofiller(usableT, usableU);
-
-    });
 });
 
-// All the modal functionality has to be a callback function from the chrome.tabs.query
-function autofiller(usableT, usableU) {
+
+/**
+ *  Creates a new bookmark given a bookmark title
+ *  and url.
+ *
+ *  @param {string} title - The title of the page being
+ *      bookmarked.
+ *  @param {string} url - The url of the page being
+ *      bookmarked.
+ *  @return {undefined}
+ *
+ */
+function autofiller(title, url) {
+    console.log("Entering autofiller in modal.js...");
     // When the user clicks the 'Bookmark' button,
     btn.onclick = function() {
         // Display the modal
         modal.style.display = "block";
         // Autofill the site Title as the bookmark name
-        document.getElementById("newName").value = usableT;
+        document.getElementById("newName").value = title;
     }
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
@@ -70,8 +103,8 @@ function autofiller(usableT, usableU) {
     // When a user clicks the Create button, save a new bookmark
     btn2.onclick = function() {
         chrome.bookmarks.create({'parentId': null,
-                                 'title':    usableT,
-                                 'url':      usableU
+                                 'title':    title,
+                                 'url':      url
                                 });
     }
 }
