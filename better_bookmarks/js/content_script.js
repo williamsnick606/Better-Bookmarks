5ff27f3b0b2b29c18550e6afdbb1e66e436f2f77
BarryManifold <bjmcgrat@ucsc.edu>
2019-07-20 20:06:00 -0700

    Made comment about how the file isn't being used currently.

--- better_bookmarks/js/content_script.js
+++ better_bookmarks/js/content_script.js
@@ -1,4 +1,5 @@
 /**
+        alert("currentTab = " + currentTab);
  * file        : content_script.js
  * description : This file contains code that grabs, whenever
  *               available, the title, description, and url of
@@ -8,53 +9,34 @@
  *       switches back to the first tab, the page information
  *       for the first tab is lost.
  *
- *       A non-optimal temporary solution to this, is saving
- *       each tab id and associating the page data with the
- *       corresponding tab id.  For example,
- *
- *           chrome.storage.sync.set({ _tabId: { title: pageTitle
- *                                               ...
- *                                             }
- *                                   });
- *
- *       Then in modal.js, we would run:
- *
- *           
- *           let currentTabId;
- *           chrome.tabs.query({ active: true, currentWindow: true},
- *                             (tabs) => {
- *               let currentTab = tabs[0];
- *               currentTabId   = currentTab.id;
- *           });
- *           let pageTitle;
- *           let pageURL;
- *           ...
- *           chrome.storage.sync.get(["_" + currentTabId],
- *                                   (result) => {
- *               pageTitle = result.title
- *               pageURL   = result.url
- *               ...
- *           });
- *           ...
- *
  */
+
 const title = document.getElementsByTagName("title")[0];
+const url   = document.URL;
 const metas = document.getElementsByTagName("meta");
-const url = document.URL;
-var description;
+
+let pageDescription;
 for (let i = 0; i < metas.length; i++) {
     const meta = metas[i];
     // Some websites don't conform to the norm, thus requiring toLowerCase()
     if (meta.name.toLowerCase() == "description"){
-        description = meta.content;
+        pageDescription = meta.content;
     }
 }
+
 console.log("Got title = " + title + " | " +
-            "description = " + description);
-if(!description){
-    description = "";
+            "description = " + pageDescription);
+
+/*
+if(!pageDescription){
+    pageDescription = "";
 }
 chrome.storage.sync.set({ title: title.innerText
                         , desc: description
                         , url: url
                         });
+*/
+{ title       : pageTitle
+, description : pageDescription
+, url         : pageURL
+};
