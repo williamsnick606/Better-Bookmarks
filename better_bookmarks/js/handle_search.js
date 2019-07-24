9196c10a2abfa4bd0d728d9b996fcc02cde27b2f
BarryManifold <bjmcgrat@ucsc.edu>
2019-07-24 02:04:38 -0700

    Don't need these files anymore.

--- better_bookmarks/js/handle_search.js
+++
@@ -1,9 +0,0 @@
-/*
- * file        : handle_search.js
- * description : This file searches the user's bookmarks and folders
- *               in the GUI and returns a match
- *
- */
-
-const userInput = document.getElementById("searchBar");
-alert("input = " + userInput.value);
